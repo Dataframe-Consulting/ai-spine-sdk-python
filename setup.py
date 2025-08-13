703c5e17ffca7d6c1c00f770471ac4152f7dbc5a
VictorBenitoGR <victorbenitogr@gmail.com>
2025-08-13 12:15:17 -0600

    chore: Actualizar la versión mínima de Python requerida a 3.9 en setup.py

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -38,7 +36,7 @@ setup(
         "Operating System :: OS Independent",
         "Typing :: Typed",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "requests>=2.28.0,<3.0.0",
         "typing-extensions>=4.0.0;python_version<'3.8'",
